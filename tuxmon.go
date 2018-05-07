12dddc2f745f8c714c637cf0949f6c664c768c6f
Mike Ripley <ripleymj@jmu.edu>
2018-05-06 21:44:19 -0400

    Invoke tmadmin on each iteration

--- tuxmon.go
+++ tuxmon.go
@@ -1,8 +1,8 @@
 package main
 
+import "bufio"
 import "fmt"
 import "io"
-import "bufio"
 import "os"
 import "os/exec"
 import "strings"
@@ -10,70 +10,67 @@ import "time"
 
 func main() {
 
-    tuxCmd := exec.Command("tmadmin", "-r");
-
     env := os.Environ()
     tuxConfig := "/home/psadm2/psft/pt/8.56/appserv/APPDOM/PSTUXCFG"
     env = append(env, fmt.Sprintf("TUXCONFIG=%s", tuxConfig))
-    tuxCmd.Env = env
-    tuxIn, _ := tuxCmd.StdinPipe()
-    tuxOut, _ := tuxCmd.StdoutPipe()
-    oBuf := bufio.NewReader(tuxOut)
-    tuxCmd.Start()
+
+    for {
+        tuxCmd := exec.Command("tmadmin", "-r");
+
+        tuxCmd.Env = env
+
+        tuxIn, _ := tuxCmd.StdinPipe()
+        tuxOut, _ := tuxCmd.StdoutPipe()
+        oBuf := bufio.NewReader(tuxOut)
     
-    go tuxWrite(tuxIn)
-    doTuxLoop := true
+        tuxCmd.Start()
 
-    for doTuxLoop {
-        appendMessage := true
+        tuxIn.Write([]byte("verbose on\n"))
+        tuxIn.Write([]byte("page off\n"))
 
-        message,_ := oBuf.ReadString('\n')
-        message = strings.TrimLeft(message, " >")
+        tuxIn.Write([]byte("psr\n"))
+        tuxIn.Write([]byte("pq\n"))
+        tuxIn.Write([]byte("pclt\n"))
+        tuxIn.Write([]byte("quit\n"))
 
-        if (strings.Index(message, "Group ID:") == 0) {
-            fmt.Println("psr message")
-        } else if (strings.Index(message, "Prog Name:") == 0) {
-            fmt.Println("pq message")
-        } else if (strings.Index(message, "LMID:") == 0) {
-            fmt.Println("pclt message")
-        }
+        moreMessages := true
 
-        for appendMessage {
-            line, err := oBuf.ReadString('\n')
-            line = strings.TrimLeft(line," >")
-            if(err != nil) {
-                fmt.Println(err)
-                break
+        MessageRead:
+        for moreMessages {
+            message,_ := oBuf.ReadString('\n')
+            message = strings.TrimLeft(message, " >")
+    
+            if (strings.Index(message, "Group ID:") == 0) {
+                fmt.Println("psr message")
+            } else if (strings.Index(message, "Prog Name:") == 0) {
+                fmt.Println("pq message")
+            } else if (strings.Index(message, "LMID:") == 0) {
+                fmt.Println("pclt message")
             }
-
-            if(line == "\n") {
-                appendMessage = false
-            } else {
-                //fmt.Print(line)
-               message += line 
+    
+            appendLine := true
+            for appendLine {
+                line, err := oBuf.ReadString('\n')
+                line = strings.TrimLeft(line," >")
+                if(err != nil ) {
+                    if(err != io.EOF) {
+                        fmt.Printf("Error: %s\n", err)
+                    }
+                    moreMessages = false
+                    break MessageRead
+                }
+    
+                if(line == "\n") {
+                    appendLine = false
+                } else {
+                   message += line 
+                }
             }
+            fmt.Print(message)
+            fmt.Println("----")
         }
-        fmt.Print(message)
-        fmt.Println("----")
-        //fmt.Println("Done with loop")
-        //doTuxLoop = false
+        time.Sleep(1 * time.Second)
+        tuxIn.Close()
+        tuxCmd.Wait()
     }
-
-    tuxCmd.Wait()
 } 
-
-func tuxWrite(pipe io.WriteCloser) {
-    pipe.Write([]byte("verbose on\n"))
-    pipe.Write([]byte("page off\n"))
-
-    for i := 0; i < 3; i++ {
-        pipe.Write([]byte("psr\n"))
-        pipe.Write([]byte("pq\n"))
-        pipe.Write([]byte("pclt\n"))
-        time.Sleep(5 * time.Second)
-        //fmt.Println(s)
-    }
-
-    pipe.Write([]byte("quit\n"))
-    pipe.Close()
-}
